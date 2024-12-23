b29a0c4d602467360b20415c19269f134422c265
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-12-23 17:59:49 -0500

    fixed build errors

--- src/app/api/openai/dall-e/route.ts
+++ src/app/api/openai/dall-e/route.ts
@@ -9,17 +9,12 @@ const openai = new OpenAI({
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
 
-export async function POST(req: Request, res: Response) {
-  if (req.method !== 'POST') {
-    return new Response('Method not allowed', {
-      status: 405,
-    });
-  }
-
+export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
-  const body = await req.text();
-  const { prompt } = JSON.parse(body);
+  const body = await req.json();
+  const { prompt } = body;
   const finalPrompt = `Create me an image related to the following content ---- ${prompt}`
+
   try {
     const response = await openai.images.generate({
       model: "dall-e-3",
@@ -27,15 +22,14 @@ export async function POST(req: Request, res: Response) {
       n: 1,
       size: "1024x1024",
     });
+
     const imageUrl = response?.data?.[0]?.url;
     console.log('imageUrl:', imageUrl);
-    return NextResponse.json({ imageUrl },{
-      status:200
-    });
+    
+    return NextResponse.json({ imageUrl }, { status: 200 });
   } catch (error: any) {
     console.error(error);
-    return new Response(error?.message || error?.toString(), {
-      status: 500,
-    })
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
+
